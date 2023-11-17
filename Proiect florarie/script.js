495799c594ba1d1d466db59b36ed3fefbf069855
Claudia-Florentina Olaru <138529156+Claudita8@users.noreply.github.com>
2023-11-17 17:18:52 +0200

    Add files via upload

---
+++ Proiect florarie/script.js
@@ -0,0 +1,10 @@
+function changeStyle(element) {
+  element.style.backgroundColor = '#FFDEE7';
+  element.style.color = 'black';
+}
+
+function resetStyle(element) {
+  element.style.backgroundColor = '';
+  element.style.color = '';
+}
+
